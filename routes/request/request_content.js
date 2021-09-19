f0156544d028ce8907b523edb3be46bac1ef2126
yejin <kyj1860@naver.com>
2021-09-19 15:28:52 +0900

    매칭 기능 추가

--- routes/request/request_content.js
+++ routes/request/request_content.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 var connection = require('../connection');
 
 router.get('/:request_code', function (req, res) {
     var id = req.user;
     var rcode = req.params.request_code;
-    var sql = 'select * from request where request_code = ?';
+    var mvalue = null;
+    var matching;
+    var sql1 = 'select * from matching where request_code = ?';
+    var sql2= 'select * from request where request_code = ?';
 
-    connection.query(sql, rcode, function(err, result) {
+    connection.query(sql1, rcode, function(err, result) {
         if (err) throw err;
-        res.render('request_content', {'id' : id, value : result[0]});
+        if (result != '') mvalue = 1;
+        matching = result;
+    });
+    connection.query(sql2, rcode, function(err, result) {
+        if (err) throw err;
+        res.render('request_content', {'id' : id, request : result[0], matching : matching[0], mvalue : mvalue});
     });
 });
 
