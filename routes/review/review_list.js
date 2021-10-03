4a0daf2cef182420d6544ddee31c54c2079f94cb
yejin <kyj1860@naver.com>
2021-10-03 18:01:26 +0900

    후기 조회, 요청 작성 시간 수정

--- routes/review/review_list.js
+++ routes/review/review_list.js
@@ -4,11 +4,20 @@ var connection = require('../connection');
 
 router.get('/', function(req, res){
     var id = req.user;
-    var sql = 'select * from request where request_code in (select request_code from matching where matching_code in (select matching_code from review where review_writer = ?))';
+    var userscore = 0;
+    var sql1 = 'select review_score from review where user_id = ?';
+    var sql2 = 'select * from request where request_code in (select request_code from matching where matching_code in (select matching_code from review where review_writer = ?))';
 
-    connection.query(sql, id, function(err, result) {
+    connection.query(sql1, id, function(err, result) {
         if (err) throw err;
-        res.render('review_list', {id : id, value : result});
+        result.forEach(element => {
+            userscore = userscore + element.review_score;
+        });
+        userscore = Math.round((userscore / result.length) * 100) / 100;
+    });
+    connection.query(sql2, id, function(err, result) {
+        if (err) throw err;
+        res.render('review_list', {id : id, userscore : userscore, value : result});
     });
 });
 
