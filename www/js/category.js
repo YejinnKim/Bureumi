01ff58ebf5c954d8d9390707504be5d78ec57bbf
yejin <kyj1860@naver.com>
2021-10-08 18:11:58 +0900

    사진 업로드 기능 추가(프로필, 요청 글)

--- www/js/category.js
+++ www/js/category.js
@@ -1,6 +1,6 @@
 $(function() {
-    var category = '기타';
-
+    var category = decodeURI(document.location.href).substr(31);
+    
     $('#category_opt').val(category).attr('selected', 'selected');
     
     $('#category_opt').on('change', function() {
