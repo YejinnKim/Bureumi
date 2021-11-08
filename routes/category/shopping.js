715a21a5282eb76ad0477ec5204ba28905f884f7
HanWoongCho <cho5678zzz@gmail.com>
2021-11-08 18:21:06 +0900

    11.08 search페이지, category페이지 오류 수정 및 기타 페이지 경로 수정

--- routes/category/shopping.js
+++ routes/category/shopping.js
@@ -11,13 +11,14 @@ router.get('/:page',async function (req, res) {
     var search_category = req.session.search_category
     var category_page = req.session.category_page;
     var total_page = Math.ceil(search_category.length / 5)
-
+    var cag_flag = '3';
     res.render('category', {
         search: search_category,
         category : category_page,
         total_page: total_page,
         page: page,
-        length: search_category.length - 1
+        length: search_category.length - 1,
+        cag_flag : cag_flag
     });
 
 
